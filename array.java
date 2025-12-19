025c51a5aec6256b83b5847fac137f7421cb2352
munyirichrispine <munyirichrispine@gmail.com>
2025-12-19 15:13:48 +0300

    Create array.java

---
+++ array.java
@@ -0,0 +1,16 @@
+public class Array {
+    public static void main(String[] args) {
+
+        int[] numbers = {5, 10, 15, 20, 25};
+
+        int sum = 0;
+
+        System.out.println("Array elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+            sum += numbers[i];
+        }
+
+        System.out.println("Sum of elements: " + sum);
+    }
+}
